7ac238a74f03eeae0236ec516e014ec4443f0d76
Maria Alice <maria.a.costa30@aluno.senai.br>
2026-03-09 14:44:34 -0300

    teste 5

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
+let taskCount = 0;
 
 function addTask() {
-    const input = document.getElementById('taskInput');
-    const taskText = input.value.trim();
+    const taskInput = document.getElementById('taskInput');
+    const profInput = document.getElementById('profInput');
+    const reviewInput = document.getElementById('reviewInput');
+    const taskList = document.getElementById('taskList');
 
-    if (taskText === "") {
-        alert("Por favor, digite uma tarefa!");
+    if (taskInput.value === "") {
+        alert("Ei, Brawler! Dê um nome ao seu trabalho!");
         return;
     }
 
-    const taskList = document.getElementById('taskList');
+    const card = document.createElement('div');
+    card.className = 'task-card';
 
-    const li = document.createElement('li');
-    
-    li.innerHTML = `
-        <span onclick="toggleTask(this)">${taskText}</span>
-        <span class="delete-btn" onclick="removeTask(this)">✖</span>
+    card.innerHTML = `
+        <button class="delete-btn" onclick="removeTask(this)">FINALIZAR</button>
+        <h3>🏆 ${taskInput.value}</h3>
+        <p>👤 Professor(a): ${profInput.value || 'Não informado'}</p>
+        <div class="review-box">
+            <strong>📝 REVISÃO:</strong><br>
+            ${reviewInput.value || 'Nenhuma anotação...'}
+        </div>
     `;
 
-    taskList.appendChild(li);
+    taskList.appendChild(card);
     
-    input.value = "";
+    taskCount++;
+    updateCount();
+    taskInput.value = "";
+    profInput.value = "";
+    reviewInput.value = "";
 }
 
-function toggleTask(element) {
-    element.parentElement.classList.toggle('completed');
+function removeTask(btn) {
+    btn.parentElement.remove();
+    taskCount--;
+    updateCount();
 }
-function removeTask(element) {
-    element.parentElement.remove();
+
+function updateCount() {
+    document.getElementById('count').innerText = taskCount;
 }
